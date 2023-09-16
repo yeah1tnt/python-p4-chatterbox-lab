e35fb2999fe63a82db8127a08c373e9c2d1ed056
Tong Tang <pichip22@hotmail.com>
2023-09-16 18:07:15 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,53 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.all()
+        message_serialized = [message.to_dict() for message in messages]
 
-@app.route('/messages/<int:id>')
+        response = make_response(jsonify(message_serialized),200)
+        return response
+    elif request.method == 'POST':
+        body = request.get_json()
+        message = Message(
+            body = body.get('body'), 
+            username = body.get('username'),
+            )
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(jsonify(message_dict),200)
+
+        return response
+    
+
+@app.route('/messages/<int:id>', methods=['GET','PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        message_serialized = message.to_dict()
+
+        response = make_response(jsonify(message_serialized),200)
+        return response
+    elif request.method == 'PATCH':
+        message = Message.query.filter_by(id=id).first()
+        message.body = request.get_json().get('body')
+
+        db.session.add(message)
+        db.session.commit()
+        message_serialized = message.to_dict()
+        response = make_response(jsonify(message_serialized),200)
+        return response
+    elif request.method == 'DELETE':
+        message = Message.query.filter_by(id=id).first()
+        db.session.delete(message)
+        db.session.commit()
+        response = make_response('',200)
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
